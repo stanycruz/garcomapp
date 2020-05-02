72a70f0eef5c5a5ccf93aa4445391e545a4d9395
Stanislaw Cruz <stanycruz@outlook.com>
2020-05-02 18:17:46 -0300

    Plugin Barcode Scanner.

--- www/js/app.js
+++ www/js/app.js
@@ -34,9 +34,29 @@ $('.acao-limpar').on('click', function () {
     $('.badge').remove();
 });
 
-
-
-
-
-
-
+$('.scan-qrcode').click(function (e) {
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            if (result.text) {
+                Materialize.toast('Mesa ' + result.text, 2000);
+                $('#numero-mesa').val(result.text);
+            }
+        },
+        function (error) {
+            Materialize.toast('Erro ' + error, 2000, 'red-text');
+        },
+        {
+            preferFrontCamera: false, // iOS and Android
+            showFlipCameraButton: true, // iOS and Android
+            showTorchButton: true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt: "Coloque o QR Code dentro da área de escaneamento.", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats: "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation: "portrait", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations: true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }
+    );
+});
